e26e7c8913599fa83af64aedaa85dd215013c43f
Jeffrey Miller <miller.s.jeffrey@gmail.com>
2019-08-13 15:07:44 -0500

    Replace game object tag conditionals with calls to CompareTag

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -8,9 +8,7 @@ public class BallController : MonoBehaviour
     public float MaxYSpeed = 2.0f;
 
     private Vector2 currentVelocity { get; set; }
-
     private Rigidbody2D ballRigidBody;
-    private Collider2D ballCollider;
 
     // Difficulty Multiplier - Is it a static product or no?
 
@@ -18,7 +16,6 @@ public class BallController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        ballCollider = GetComponent<Collider2D>();
         ballRigidBody = GetComponent<Rigidbody2D>();
         currentVelocity = new Vector2(MinXSpeed, -MinYSpeed);
         ballRigidBody.velocity = currentVelocity;
@@ -26,95 +23,36 @@ public class BallController : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        var tag = collision.gameObject.tag;
-        switch (tag)
+        if (collision.gameObject.CompareTag("Paddle") || collision.gameObject.CompareTag("UpperLimit"))
         {
-            case "Paddle":
-                Debug.Log("Hit the Paddle!");
-                // What side of the paddle has it hit - need a reference to the paddle
-                ballRigidBody.velocity = new Vector2(currentVelocity.x, -currentVelocity.y);
-                currentVelocity = new Vector2(currentVelocity.x, -currentVelocity.y);
-                //Debug.Log(ballRigidBody.velocity);
-                break;
-            case "RightLimit":
-                Debug.Log("Hit the SideLimit!");
-                ballRigidBody.velocity = new Vector2(-currentVelocity.x, currentVelocity.y);
-                currentVelocity = new Vector2(-currentVelocity.x, currentVelocity.y);
-                //Debug.Log(ballRigidBody.velocity);
-                break;
-            case "LeftLimit":
-                Debug.Log("Hit the SideLimit!");
-                ballRigidBody.velocity = new Vector2(-currentVelocity.x, currentVelocity.y);
-                currentVelocity = new Vector2(-currentVelocity.x, currentVelocity.y);
-                //Debug.Log(ballRigidBody.velocity);
-                break;
-            case "UpperLimit":
-                Debug.Log("Hit the UpperLimit!");
-                ballRigidBody.velocity = new Vector2(currentVelocity.x, -currentVelocity.y);
-                currentVelocity = new Vector2(currentVelocity.x, -currentVelocity.y);
-                //Debug.Log(ballRigidBody.velocity);
-                break;
-            default:
-                break;
+            Debug.Log("Hit the Paddle!");
+            // What side of the paddle has it hit - need a reference to the paddle
+            ballRigidBody.velocity = new Vector2(currentVelocity.x, -currentVelocity.y);
+            currentVelocity = new Vector2(currentVelocity.x, -currentVelocity.y);
+            return;
+            //Debug.Log(ballRigidBody.velocity);
+        }
+        if (collision.gameObject.CompareTag("RightLimit"))
+        {
+            Debug.Log("Hit the SideLimit!");
+            ballRigidBody.velocity = new Vector2(-currentVelocity.x, currentVelocity.y);
+            currentVelocity = new Vector2(-currentVelocity.x, currentVelocity.y);
+            //Debug.Log(ballRigidBody.velocity);
+            return;
+        }
+        if (collision.gameObject.CompareTag("LeftLimit"))
+        {
+            Debug.Log("Hit the SideLimit!");
+            ballRigidBody.velocity = new Vector2(-currentVelocity.x, currentVelocity.y);
+            currentVelocity = new Vector2(-currentVelocity.x, currentVelocity.y);
+            //Debug.Log(ballRigidBody.velocity);
+            return;
         }
     }
-
-    //private void OnTriggerEnter2D(Collider2D collision)
-    //{
-    //    ContactPoint2D[] contacts = new ContactPoint2D[10];
-    //    int contactCount = ballCollider.GetContacts(contacts);
-    //    for (int i = 0; i < contactCount; i++)
-    //    {
-    //        Debug.Log(contacts[i]);
-    //        ContactPoint2D contact = contacts[i];
-    //    }
-    //    string tag = collision.tag;
-    //    switch (tag)
-    //    {
-    //        case "Paddle":
-    //            Debug.Log("Hit the Paddle!");
-    //            // What side of the paddle has it hit - need a reference to the paddle
-    //            ballRigidBody.velocity = new Vector2(ballRigidBody.velocity.x, -ballRigidBody.velocity.y);
-    //            break;
-    //        case "SideLimit":
-    //            Debug.Log("Hit the SideLimit!");
-    //            ballRigidBody.velocity = new Vector2(-ballRigidBody.velocity.x, ballRigidBody.velocity.y);
-    //            break;
-    //        case "UpperLimit":
-    //            Debug.Log("Hit the UpperLimit!");
-    //            ballRigidBody.velocity = new Vector2(ballRigidBody.velocity.x, -ballRigidBody.velocity.y);
-    //            break;
-    //        default:
-    //            break;
-    //    }
-    //}
-
-    //private void OnCollisionEnter2D(Collision2D collision)
-    //{
-    //    string tag = collision.collider.tag;
-    //    switch (tag)
-    //    {
-    //        case "Paddle":
-    //            Debug.Log("Hit the Paddle!");
-    //            // What side of the paddle has it hit - need a reference to the paddle
-    //            ballRigidBody.velocity = new Vector2(ballRigidBody.velocity.x, -ballRigidBody.velocity.y);
-    //            break;
-    //        case "SideLimit":
-    //            Debug.Log("Hit the SideLimit!");
-    //            ballRigidBody.velocity = new Vector2(-ballRigidBody.velocity.x, ballRigidBody.velocity.y);
-    //            break;
-    //        case "UpperLimit":
-    //            Debug.Log("Hit the UpperLimit!");
-    //            ballRigidBody.velocity = new Vector2(ballRigidBody.velocity.x, -ballRigidBody.velocity.y);
-    //            break;
-    //        default:
-    //            break;
-    //    }
-    //}
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        Debug.Log(ballRigidBody.velocity);
+        //Debug.Log(ballRigidBody.velocity);
     }
 }
